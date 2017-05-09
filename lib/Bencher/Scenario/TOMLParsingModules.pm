01c1ca121bb5e09df65f06b41f6e6c78d9fd57c0
perlancar (on pc-office) <perlancar@gmail.com>
2017-05-09 19:47:05 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/TOMLParsingModules.pm
@@ -0,0 +1,40 @@
+package Bencher::Scenario::TOMLParsingModules;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use File::ShareDir::Tarball qw(dist_dir);
+
+our $scenario = {
+    summary => 'Benchmark TOML parsing modules',
+    modules => {
+        # minimum versions
+    },
+    extra_modules => ['File::Slurper'],
+    participants => [
+        {
+            module => 'TOML::Parser',
+            code_template => 'state $parser = TOML::Parser->new; $parser->parse(File::Slurper::read_text(<filename>))',
+        },
+    ],
+
+    datasets => [
+    ],
+};
+
+my $dir = dist_dir('TOML-Examples')
+    or die "Can't find share dir for TOML-Examples";
+for my $filename (glob "$dir/examples/iod/extra-bench-*.toml") {
+    my $basename = $filename; $basename =~ s!.+/!!;
+    push @{ $scenario->{datasets} }, {
+        name => $basename,
+        args => {filename => $filename},
+    };
+}
+
+1;
+# ABSTRACT:
